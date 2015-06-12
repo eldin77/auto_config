d2f1ee9e07bfaab7c7ef1995c78c0cd7417e06ad
kobe <gokuma77@gmail.com>
2015-06-12 11:10:29 +0900

    help modified

--- logo.py
+++ logo.py
@@ -4,7 +4,7 @@ def print_help():
     print "1. Delete Lswitch: NSX Delete Logical Switch Delete                     command: 1, Delete"
     print "2. Create Lswitch: NSX Create Logical Switch, Port, Port ,attachment    command: 2, Create"
     print "4. Backup Config : NSX config backup.                                   command: 4, backup"
-    print "5. Restore : NSX config restore. not implementation.                    command: 5, restore"
+    print "5. Restore : NSX config restore.                                        command: 5, restore"
     print "6. Clear : all clear nsx config.                                        command: 6, clear"
     print "H. Help.                                                                command: H, help, h"
     print "Q. Quit.                                                                command: q, Q, quit"
